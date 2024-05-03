8bd6dbe2fcf03f500f16c273e4ba158186aca8ea
deeg-p23 <23diegopereyra@gmail.com>
2024-05-03 00:46:31 -0700

    Began base movement for combat-state

--- Assets/Scripts/Player/BaseMovement.cs
+++ Assets/Scripts/Player/BaseMovement.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class BaseMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private CharacterController controller;
+
+    public float moveSpeed;
+    public float jumpForce;
+    public float gravityScale;
+
+    Vector3 movementVector;
+
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // COMBAT-STATE MOVEMENT [LEFT/RIGHT/JUMP]
+        float xDisplacement = 0f;
+
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D)) {
+            xDisplacement = -1f;
+        }
+        if (!Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D)) {
+            xDisplacement = 1f;
+        }
+
+        movementVector = new Vector3(xDisplacement * moveSpeed, movementVector.y, 0);
+
+        if (controller.isGrounded) {
+            if (Input.GetKeyDown(KeyCode.W)) {
+                movementVector.y = jumpForce;
+            }
+        }
+
+        movementVector.y = movementVector.y + (Physics.gravity.y * gravityScale);
+
+        controller.Move(movementVector * Time.deltaTime);
     }
 }
