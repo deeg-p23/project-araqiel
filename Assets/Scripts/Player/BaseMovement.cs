c1f384c0e4c15e03d59da8cddb9634e8fe44040d
deeg-p23 <23diegopereyra@gmail.com>
2024-05-21 23:22:07 -0700

    added scale/shift/zoom factor to make accurate&dynamic playercam
    next to work on:
    
    - downkey (gravity accelerate)
    - aiming + dynamic cursorUI

--- Assets/Scripts/Player/BaseMovement.cs
+++ Assets/Scripts/Player/BaseMovement.cs
@@ -44,16 +44,27 @@ public class BaseMovement : MonoBehaviour
 
         controller.Move(movementVector * Time.deltaTime);
 
-        // MOUSE MOVEMENT [alter camera view]
+        // CAMERA MOVEMENT + AIM (mouse position)
         Vector3 mousePos = Input.mousePosition;
 
-        float origin_relative_mousex = mousePos.x - (Screen.width / 2);
-        float origin_relative_mousey = mousePos.y - (Screen.height / 2);
+        // divide screen width/height by 2 to get the edge-to-origin length
+        float origin_relative_mousex = mousePos.x - (Screen.width / 2f);
+        float origin_relative_mousey = mousePos.y - (Screen.height / 2f);
         
         Vector3 newCamPos = new Vector3();
-        newCamPos.x = origin_relative_mousex / (Screen.width / 3);
-        newCamPos.y = origin_relative_mousey / (Screen.height / 3) + 2f;
-        newCamPos.z = -14f;
+
+        newCamPos.x = (Mathf.Abs(origin_relative_mousex) < (Screen.width / 2f)) ? (origin_relative_mousex) : ((origin_relative_mousex < 0f) ? (-1f * Screen.width / 2f) : (Screen.width / 2f));
+        newCamPos.y = (Mathf.Abs(origin_relative_mousey) < (Screen.height / 2f)) ? (origin_relative_mousey) : ((origin_relative_mousey < 0f) ? (-1f * Screen.height / 2f) : (Screen.height / 2f));
+        
+        // THE FOLLOWING ARE MAGIC NUMBERS FOR NOW, MAKE THEM CHANGEABLE VARIABLES LATER ON FOR CUTSCENES?
+        // 130f is a cam scale factor, 
+        // 3f is a cam y-shift factor,
+        // -18f is a cam zoom factor
+
+        newCamPos.x /= 130f;
+        newCamPos.y /= 130f;
+        newCamPos.y += 3f;
+        newCamPos.z = -16f;
 
         playerCamera.transform.position = transform.position + newCamPos;
     }
